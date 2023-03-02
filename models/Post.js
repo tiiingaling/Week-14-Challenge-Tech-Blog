b65f44addc34d16eca80e597e8a88749e0eb3893
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-03-02 22:15:53 +0000

    add relationship to comment in posts

--- models/Post.js
+++ models/Post.js
@@ -30,6 +30,13 @@ Post.init(
         key: 'id',
       },
     },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'comment',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
